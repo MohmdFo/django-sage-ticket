4c73c2ccfbf796bc5699d41fb1bf5d350612f871
MohmdFo <mohammad.fotouhi80@gmail.com>
2024-12-04 22:27:01 +0330

    feat(faq): add faq model admin and transation
    - update package version support

--- sage_ticket/admin/__init__.py
+++ sage_ticket/admin/__init__.py
@@ -2,10 +2,13 @@ from .attachment import AttachmentAdmin
 from .comment import CommentAdmin
 from .department import DepartmentAdmin
 from .issue import IssueAdmin
+from .faq import FaqCategoryAdmin, FaqAdmin
 
 __all__ = [
     "AttachmentAdmin",
     "CommentAdmin",
     "DepartmentAdmin",
     "IssueAdmin",
+    "FaqCategoryAdmin",
+    "FaqAdmin",
 ]
---
+++ sage_ticket/models/faq.py
@@ -0,0 +1,76 @@
+"""
+FAQ Model Definition
+"""
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from sage_tools.mixins.models.base import TimeStampMixin, TitleSlugMixin
+
+
+class FaqCategory(TitleSlugMixin, TimeStampMixin):
+    """
+    Portfolio (Gallery) category
+    """
+
+    objects = models.Manager()
+
+    class Meta:
+        """Meta Information"""
+
+        verbose_name = _("FAQ Category")
+        verbose_name_plural = _("FAQ Categories")
+
+    def __repr__(self):
+        return f"<FAQ Category: {self.title}>"
+
+
+class Faq(TimeStampMixin):
+    """
+    FAQ Model.
+
+    Represents a Frequently Asked Question (FAQ) entry. Each entry consists of a
+    question and its corresponding answer. This model is useful for managing
+    common queries that users might have, providing a quick and accessible resource
+    for both site visitors and administrators.
+    """
+
+    question = models.CharField(
+        _("Question"),
+        max_length=150,
+        help_text=_(
+            "Enter the FAQ question. Keep it clear and concise to aid easy understanding."
+        ),
+        db_comment="The question of the FAQ. Limited to 80 characters.",
+    )
+
+    answer = models.TextField(
+        _("Answer"),
+        help_text=_(
+            "Provide a detailed answer to the FAQ question. Aim for clarity and completeness."
+        ),
+        db_comment="Detailed answer to the FAQ question. Limited to 180 characters.",
+    )
+
+    category = models.ForeignKey(
+        "FaqCategory",
+        on_delete=models.CASCADE,
+        related_name="faqs",
+        verbose_name=_("Category"),
+        help_text=_("Choose the category of the faq."),
+        db_comment="The category to which the faq belongs.",
+    )
+
+    class Meta:
+        """Meta Information"""
+
+        verbose_name = _("FAQ")
+        verbose_name_plural = _("FAQ")
+        db_table_comment = "Table storing frequently asked questions and their answers for user reference."
+
+    def __str__(self):
+        """Cast to String"""
+        return str(self.question)
+
+    def __repr__(self):
+        """Object Representation"""
+        return f"<FAQ: {self.question}>"
